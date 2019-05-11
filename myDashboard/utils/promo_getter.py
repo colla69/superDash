0021d64e8d6c6c527559b076e9a2b6b29daf0884
colla69 <colla69@gmail.com>
2019-05-11 13:48:44 +0200

    promo getter login funcitonal

--- myDashboard/utils/promo_getter.py
+++ myDashboard/utils/promo_getter.py
@@ -1,31 +1,23 @@
-from lxml import html
-import requests
-from bs4 import BeautifulSoup
 from superDash.psw import uni_psw
+import mechanicalsoup
 
 # uebungen
 def get_uebungen():
-    payload = {'hident11': "a.colarieti@campus.lmu.de", 'hident12': uni_psw()}
-    session = requests.Session()
-    r = session.post("https://uni2work.ifi.lmu.de/")
-    cookies = dict(r.cookies)
-    headers = dict(r.headers)
+    browser = mechanicalsoup.StatefulBrowser()
+    browser.open('https://uni2work.ifi.lmu.de')
+    browser.follow_link("login")
+    # print(browser.get_current_page())
+    form = browser.select_form('form[action="https://uni2work.ifi.lmu.de/auth/page/LDAP"]')
+    form['f1'] = "a.colarieti@campus.lmu.de"
+    form['f2'] = uni_psw()
+    form.print_summary()
+    resp = browser.submit_selected()
 
-    csrftoken = cookies['XSRF-TOKEN']
-    headers['X-XSRF-TOKEN'] = csrftoken
-    headers['ContentType'] = "application/x-www-form-urlencoded"
+    browser.open('https://uni2work.ifi.lmu.de/course/S19/IfI/ProMo/ex')
 
-    login = session.post("https://uni2work.ifi.lmu.de/auth/page/LDAP", data=payload, headers=session.headers, cookies=session.cookies)
-    print("login  "+str(login.status_code))
+    #res = { browser.get_current_page()}
 
-    csrftoken = cookies['XSRF-TOKEN']
-    headers['X-XSRF-TOKEN'] = csrftoken
-
-    page = session.get("https://uni2work.ifi.lmu.de/course/S19/IfI/ProMo/ex", headers=session.headers, cookies=session.cookies)
-
-    res = {}
-
-    return res
+    return browser.get_current_page()
 
 
 ub = get_uebungen()
