ae87f5f9fe005bb93553c8d3f4f3bb1d121a91c5
colla69 <colla69@gmail.com>
2019-06-07 19:43:43 +0200

    cleanup

--- myDashboard/utils/ip_track.py
+++ myDashboard/utils/ip_track.py
@@ -1,14 +1,14 @@
-from datetime import datetime,timezone
+from datetime import datetime
 from myDashboard.models import IpLog
 
-lastupdate = datetime.now()
 
 def save_ip(new_ip):
     last_ip = get_last_ip()
+    now = datetime.now()
     if new_ip:
         if last_ip != new_ip:
-            now = datetime.now(timezone.utc)
             new_ip = IpLog.objects.create(ip=new_ip, time=now)
+
 
 def get_last_ip():
     return IpLog.objects.last().ip
