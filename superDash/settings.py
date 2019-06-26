fa0ca879dcff3bb1119c8a97fb1c3349b2de51c7
colla69 <colla69@gmail.com>
2019-06-26 16:59:20 +0200

    fix static content

--- superDash/settings.py
+++ superDash/settings.py
@@ -16,7 +16,6 @@ from superDash.psw import psw, csrf_key
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -123,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT= '/code/myDashboard/static/'
